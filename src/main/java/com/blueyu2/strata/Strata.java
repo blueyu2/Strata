944a2f8183ce80f9df646f78caafb1f36814caa8
blueyu2 <blueyu2@users.noreply.github.com>
2016-01-09 13:37:00 -0600

    Update Version

--- src/main/java/com/blueyu2/strata/Strata.java
+++ src/main/java/com/blueyu2/strata/Strata.java
@@ -20,7 +20,7 @@ import java.io.File;
 @Mod(modid = Strata.MODID, version = Strata.VERSION)
 public class Strata {
     public static final String MODID = "strata";
-    public static final String VERSION = "1.7.10-1.3";
+    public static final String VERSION = "1.7.10-1.4";
 
     @SidedProxy(serverSide = "com.blueyu2.strata.Proxy", clientSide = "com.blueyu2.strata.ProxyClient")
     public static Proxy proxy;
